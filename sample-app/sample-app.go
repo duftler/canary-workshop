6cce4d1a2e3625422b68fcd087c7d97982b5fe2b
Matt Duftler <duftler@google.com>
2019-11-14 23:25:02 -0800

    Add more metrics to sample app.

--- sample-app/sample-app.go
+++ sample-app/sample-app.go
@@ -2,8 +2,10 @@ package main
 
 import (
     "fmt"
+    "math/rand"
     "net/http"
     "os"
+    "strconv"
     "time"
 
     "github.com/joho/godotenv"
@@ -12,19 +14,53 @@ import (
     "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var httpRequestRateFloat float64 = 0;
+var httpErrorRateFloat float64 = 0;
+var jobProcessingTimeStdDevFloat float64 = 1;
+var jobProcessingTimeMeanFloat float64 = 0;
+var networkLatencyStdDevFloat float64 = 1;
+var networkLatencyMeanFloat float64 = 0;
 func recordMetrics() {
     go func() {
         for {
-            opsProcessed.Inc()
-            time.Sleep(2 * time.Second)
+            jobProcessingTime.Set(rand.NormFloat64() * jobProcessingTimeStdDevFloat + jobProcessingTimeMeanFloat)
+
+            networkLatency.Set(rand.NormFloat64() * networkLatencyStdDevFloat + networkLatencyMeanFloat)
+
+            httpRequestsToAdd := rand.NormFloat64() + httpRequestRateFloat
+            httpRequests.Add(httpRequestsToAdd)
+            httpErrors.Add(httpRequestRateFloat * httpErrorRateFloat)
+
+            time.Sleep(1 * time.Second)
         }
     }()
 }
 
 var (
-    opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-        Name: "myapp_processed_ops_total",
-        Help: "The total number of processed events",
+    httpRequests = promauto.NewCounter(prometheus.CounterOpts{
+        Name: "http_request_count",
+        Help: "HTTP requests",
+    })
+)
+
+var (
+    httpErrors = promauto.NewCounter(prometheus.CounterOpts{
+        Name: "http_error_count",
+        Help: "HTTP errors",
+    })
+)
+
+var (
+    jobProcessingTime = promauto.NewGauge(prometheus.GaugeOpts{
+        Name: "job_processing_time",
+        Help: "Job processing time",
+    })
+)
+
+var (
+    networkLatency = promauto.NewGauge(prometheus.GaugeOpts{
+        Name: "network_latency",
+        Help: "Network latency",
     })
 )
 
@@ -35,25 +71,23 @@ func init() {
 }
 
 func main() {
-    someName, exists := os.LookupEnv("SOME_NAME")
+    httpRequestRateStr, _ := os.LookupEnv("HTTP_REQUEST_RATE")
+    httpErrorRateStr, _ := os.LookupEnv("HTTP_ERROR_RATE")
+    httpRequestRateFloat, _ = strconv.ParseFloat(httpRequestRateStr, 64)
+    httpErrorRateFloat, _ = strconv.ParseFloat(httpErrorRateStr, 64)
 
-    if exists {
-  	    fmt.Println(someName)
-    } else {
-        fmt.Println("No env var set...")
-    }
+    jobProcessingTimeStdDevStr, _ := os.LookupEnv("JOB_PROCESSING_TIME_STD_DEV")
+    jobProcessingTimeMeanStr, _ := os.LookupEnv("JOB_PROCESSING_TIME_MEAN")
+    jobProcessingTimeStdDevFloat, _ = strconv.ParseFloat(jobProcessingTimeStdDevStr, 64)
+    jobProcessingTimeMeanFloat, _ = strconv.ParseFloat(jobProcessingTimeMeanStr, 64)
 
-    fmt.Printf("Hi %s...\n", someName)
+    networkLatencyStdDevStr, _ := os.LookupEnv("NETWORK_LATENCY_STD_DEV")
+    networkLatencyMeanStr, _ := os.LookupEnv("NETWORK_LATENCY_MEAN")
+    networkLatencyStdDevFloat, _ = strconv.ParseFloat(networkLatencyStdDevStr, 64)
+    networkLatencyMeanFloat, _ = strconv.ParseFloat(networkLatencyMeanStr, 64)
 
     recordMetrics()
 
     http.Handle("/metrics", promhttp.Handler())
-    http.HandleFunc("/", HelloServer)
     http.ListenAndServe(":8080", nil)
-}
-
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-    someName := os.Getenv("SOME_NAME")
-
-    fmt.Fprintf(w, "Hello, %s and %s!", r.URL.Path[1:], someName)
 }
